d2e90d5c56d7d359bdc9ed8ff0a4de8ec15e8294
eighthhiro <110251865+eighthhiro@users.noreply.github.com>
2024-08-27 16:47:58 +0800

    Add files via upload

---
+++ ascii art.py
@@ -0,0 +1,12 @@
+greet = ("hello sir! I hope you are doing well")
+introduce = ("Hebrew Miranda - A BSCS Freshman")
+print(greet)
+print("""
+      
+ ▄ .▄▄▄▄ .▄▄▄▄· ▄▄▄  ▄▄▄ .▄▄▌ ▐ ▄▌
+██▪▐█▀▄.▀·▐█ ▀█▪▀▄ █·▀▄.▀·██· █▌▐█
+██▀▐█▐▀▀▪▄▐█▀▀█▄▐▀▀▄ ▐▀▀▪▄██▪▐█▐▐▌
+██▌▐▀▐█▄▄▌██▄▪▐█▐█•█▌▐█▄▄▌▐█▌██▐█▌
+▀▀▀ · ▀▀▀ ·▀▀▀▀ .▀  ▀ ▀▀▀  ▀▀▀▀ ▀▪
+""")
+print (introduce)
